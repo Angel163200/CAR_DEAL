13dad4497e29b5f3a5859701e7b1233e40ea0eae
Angel163200 <angelmariathomas163@gmail.com>
2023-05-05 23:48:50 +0530

    commits

--- resources/views/cars/edit.blade.php
+++ resources/views/cars/edit.blade.php
@@ -118,7 +118,7 @@
                                         </div>
                                         <div class="button-wrapper">
                                             <label for="upload" class="btn btn-primary me-2 mb-4" tabindex="0">
-                                                <span class="d-none d-sm-block">Upload new photo</span>
+                                                <span class="d-none d-sm-block">Upload a new photo</span>
                                                 <i class="bx bx-upload d-block d-sm-none"></i>
                                                 <input type="file" value="{{ asset('users/cars/' . $car->picture) }}"
                                                     id="upload" name="picture"
@@ -130,7 +130,7 @@
                                                 <i class="bx bx-reset d-block d-sm-none"></i>
                                                 <span class="d-none d-sm-block">Reset</span>
                                             </button>
-                                            <p class="text-muted mb-0">Allowed JPG, GIF or PNG. Max size of 800K</p>
+                                            <p class="text-muted mb-0">Allowed JPG, GIF or PNG. Max size of 800KB</p>
                                             @error('picture')
                                                 <span class="invalid-feedback" role="alert">
                                                     <strong>{{ $message }}</strong>
